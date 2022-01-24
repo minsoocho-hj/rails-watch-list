82576a927b01b3a9a47abcc36910cc3ac837302f
minsoo cho <khg04170@gmail.com>
2022-01-24 21:20:59 +0900

    show page..

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists do
+    resources :bookmarks, only: [ :new, :create ]
+    resources :reviews, only: :create
+  end
+  resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
 end
+
+# list/id/bookmarks
+# list/id/reviews
